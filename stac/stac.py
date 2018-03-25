123ab73c36609f8b552b921b9a36617e3de42fcd
Mateusz Karcz <mateusz.karcz@interia.eu>
2018-03-25 20:26:32 +0200

    Make Windows great again. Make logo printable. (Lower our expectations in terms of Unicode support in terminal fonts.)

--- stac/stac.py
+++ stac/stac.py
@@ -128,20 +128,20 @@ def cli():
     """Browse problems and submit solutions for the STOS platform.
 
     \b
-    #define/***/ （ɺʘДʘ）ɺ/*2018*/long/**/int
+    #define/***/  ˂ɺʘДʘ˃ɺ/*2018*/long/**/int
     /**/#include  <stdio.h>/**STOS Client**/
-    #define            ¯ا_/**/        return
-    /*M*J*/           （ɺʘДʘ）ɺ        main()
-    #define            ὢ/**/(         5/100)
-    #define/*M**K*/    /**/_ا¯        ;;;};;
-    {（ɺʘДʘ）ɺ/****/v   =8*4*2*        1;for(      （ɺʘДʘ）ɺ _ɩ_ɩ_
+    #define            ¯I_/**/        return
+    /*M*J*/            ˂ɺʘДʘ˃ɺ        main()
+    #define            ὢ/***/(        5/100)
+    #define/*M**K*/    /**/_I¯        ;;;};;
+    {˂ɺʘДʘ˃ɺ/****/v    =8*4*2*        1;for(      ˂ɺʘДʘ˃ɺ _ɩ_ɩ_
     =ὢ*ὢ*1;            _ɩ_ɩ_<5        ;_ɩ_ɩ_
     ++)putc            ((*&v+=        _ɩ_ɩ_%
     (-5+7)?            (_ɩ_ɩ_+        1)/2:-
     (7+8+4)*(_ɩ_ɩ_+21  -37-1+2        *2*2*2
     )),stdout-21/37+ὢ  );{;;};        {;;;;}
 
-    ¯ا_(ὢ)_ا¯
+                      ¯I_(ὢ)_I¯
 
     """
     pass
