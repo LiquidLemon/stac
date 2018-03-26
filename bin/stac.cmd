e68eb62b69bf12a0d664a9f539ee7bd43aa620fd
Mateusz Karcz <mateusz.karcz@interia.eu>
2018-03-26 07:01:36 +0000

    Make Windows NT script work with new paths

--- bin/stac.cmd
+++ bin/stac.cmd
@@ -1,4 +1,4 @@
 @rem Shorthand script for Windows, allowing for running the STAC without
 @rem writing `python path\to\stac.py <args>`
 
-@python %~dp0\stac\stac.py %*
+@python %~dp0\..\stac\stac.py %*
